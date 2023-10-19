cc32820963f1bd4cf56f2d2c5840512fbf9df60b
alona <alonakovtun55@gmail.com>
2023-10-19 22:07:03 +0200

    post page desktop

--- wp-content/themes/rha/template-page/template-post_page.php
+++ wp-content/themes/rha/template-page/template-post_page.php
@@ -13,8 +13,44 @@ get_header();
 ?>
 
 <main id="primary" class="site-main post-page">
+    <div class="site-branding">
+        <?php
+        the_custom_logo();
+        if (is_front_page() && is_home()) :
+        ?>
+            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"></a></h1>
+        <?php
+        endif; ?>
 
-  
+        <div class="title-page">
+            <?= the_title(); ?>:
+            <span class="chapter-title"><?= the_field('chapter_title')?></span>
+        </div>
+
+    </div><!-- .site-branding -->
+
+    <div class="content-block">
+        <div class="block">
+        <?php if (have_rows('content')) : ?>
+            <?php while (have_rows('content')) : the_row();
+                $text = get_sub_field('text');
+                $image = get_sub_field('image');
+            ?> 
+
+            <div class="content">
+                <?= $text; ?>
+            </div>
+
+            <?php if($image): ?>
+                <img src="<?= $image; ?>" alt="">
+            <?php endif; ?>
+
+            <?php endwhile; ?>
+            <?php endif; ?>
+    </div>
+        </div>
+      
+
 
 
 </main>
